729c26eddba2a5a6b99935eacbfe1286d1d7fc7c
Rahul Yadav <rahulyadav989290@gmail.com>
2024-09-21 11:12:43 +0530

    rceating folder lib for database connection for mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
@@ -12,5 +12,6 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`server is running on http://localhost:5000: ${PORT} `);
 });
