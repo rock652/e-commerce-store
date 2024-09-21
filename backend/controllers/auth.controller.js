0e322762656fb0cd36ed349da46a082bddda8bfc
Rahul Yadav <rahulyadav989290@gmail.com>
2024-09-21 11:59:12 +0530

    Implement user signup functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,16 @@
+import User from "../models/user.model";
+
 export const signup = async (req, res) => {
-  res.send("sign up route called");
+  const { email, password, name } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).json({ message: "User already exist" });
+  }
+
+  const user = await User.create({ name, email, password });
+
+  res.send(201).json({ user, mesage: "User created successfully" });
 };
 
 export const login = async (req, res) => {
