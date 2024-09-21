8cbdcec7ddb6c44833064538e3dc479f702e1e9d
Rahul Yadav <rahulyadav989290@gmail.com>
2024-09-21 10:48:34 +0530

    fix(auth): Update authentication logic in auth.controller.js and auth.route.js

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,10 @@
+export const signup = async (req, res) => {
+  res.send("sign up route called");
+};
+
+export const login = async (req, res) => {
+  res.send("sign up route called");
+};
+export const logout = async (req, res) => {
+  res.send("sign up route called");
+};
