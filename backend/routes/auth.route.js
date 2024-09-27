b22ad280dd28fc496bdec5a8161496e398e70ae6
Rahul Yadav <rahulyadav989290@gmail.com>
2024-09-27 10:51:13 +0530

    completed the product routes

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -12,6 +12,6 @@ router.post("/login", login);
 router.post("/logout", logout);
 router.post("/signup", signup);
 router.post("/refresh-token", refreshToken);
-router.post("/profile", getProfile);
+// router.post("/profile", getProfile);
 
 export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js";
 import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
@@ -14,6 +17,7 @@ app.use(express.json()); //allows you parse the body data
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(PORT, () => {
   connectDB();
